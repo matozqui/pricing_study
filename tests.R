2bb4142a97952c7cd133abc16de59b45bbb293cf
matozqui <57729840+matozqui@users.noreply.github.com>
2020-02-22 18:35:38 +0100

    Delete tests.R

--- tests.R
+++
@@ -1,5 +0,0 @@
-
-context('Import process ok')
-test_that("Rows imported", expect_true(nrow(data)>1000))
-test_that("Columns imported", expect_true(ncol(data)>5))
-test_that("Type cm_name", expect_is(data$cm_name,'character'))
